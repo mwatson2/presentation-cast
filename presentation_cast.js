794eb867023527469d9016e85c2320914aa24059
mark a. foltz <mfoltz@chromium.org>
2014-10-20 18:50:56 -0700

    Implement request/join

--- presentation_cast.js
+++ presentation_cast.js
@@ -1,43 +1,60 @@
 (function() {
   // Utility for logging messages to the developer console.
   window.log = {
-    info: function(message) {console.info('[PAPI] ' + message);},
-    warn: function(message) {console.warn('[PAPI] ' + message);},
-    error: function(message) {console.error('[PAPI] ' + message);}
+    info: function(message) {console.info('[papi] ' + message);},
+    warn: function(message) {console.warn('[papi] ' + message);},
+    error: function(message) {console.error('[papi] ' + message);}
   };
 
   var presentation = {};
+
+  // Event handler for OnAvailableChangeEvent.
+  presentation.onavailablechange = null;
+
+  ////////////////////////////////////////////////////////////////////////////
+  // Bookkeeping for the polyfill.
+
+  // Map from presentation-URL|id to the corresponding PresentationSession.
+  var presentationSessions_ = {};
+  // Map from Cast session id to the corresponding presentation-URL|id.
+  var castSessions_ = {};
+  // The next presentation id to hand out (if one is not provided).
+  var nextId_ = null;
 
   // https://webscreens.github.io/slidyremote/receiver.html
   var CAST_APP_ID = '673D55D4';
   var PRESENTATION_API_NAMESPACE_ =
       'urn:x-cast:org.w3.webscreens.presentationapi.shim';
   var ORIGIN_RE_ = new RegExp('https?://[^/]+');
+  var NEXT_ID_KEY_ = 'navigator.presentation.nextId';
 
+  var getNextId_ = function() {
+    if (!nextId) {
+      // Initialize nextId.
+      if (localStorage[NEXT_ID_KEY_]) {
+        nextId = new Number(localStorage[NEXT_ID_KEY_]);
+      } else {
+        nextId = 9999;
+      }
+    }
+    nextId++;
+    localStorage[NEXT_ID_KEY_] = nextId;
+    return nextId;
+  };
 
   ////////////////////////////////////////////////////////////////////////////
   // Implementation of Presentation API at
-  // http://webscreens.github.io/presentation-api/ with some proposed changes:
-  // - Added an |id| property to PresentationSession
-  // - Added |presentationId| and |opt_onlyReconnect| parameters to
-  //   requestSession
-  presentation.requestSession = function(url, presentationId, opt_onlyReconnect) {
-    if (this.requestedSession_) {
-      console.info('There is already a presentation request pending for '
-          + this.requestedSession_.getKey());
-      return;
-    }
-
-    presentationId = presentationId ? presentationId : '';
-    var sessionKey = url + '|' + presentationId;
+  // http://webscreens.github.io/presentation-api/
+  presentation.requestSession = function(url, opt_id) {
+    var id = opt_id || getNextId_();
+    var session = new PresentationSession(url, id);
 
     // See if we already have a PresentationSession for this request.
-    session = presentationSessions_[sessionKey];
-    // Otherwise create a new PresentationSession.
-    if (!session) {
-      session = new PresentationSession(url, presentationId);
-      presentationSessions_[sessionKey] = session;
+    if (presentationSessions_[session.key_]) {
+      return presentationSessions_[session.key_];
     }
+    // Otherwise create a new session.
+    presentationSessions_[session.key_] = session;
 
     // If the PresentationSession already has a Cast session attached return it.
     if (session.hasCastSession_()) {
@@ -45,22 +62,40 @@
     } else if (session.castSessionId_) {
       // If the Cast session id is known for the PresentationSession, request
       // it.
-      //
-      // TODO(mfoltz): Keep a persistent map of session key -> cast session id
-      // for connections across pages.
-      this.requestedSession_ = session;
       chrome.cast.lookupSessionById(session.castSessionId_);
-    } else if (!opt_onlyReconnect) {
-      // Otherwise, if opt_onlyReconnect != true, request a new Cast session.
-      this.requestedSession_ = session;
+    } else {
+      // Request a new Cast session.
       chrome.cast.requestSession();
     }
-
     // Return the PresentationSession.
+    // TODO: Return a Promise that is resolved when the session is connected.
     return session;
   };
 
-  presentation.navailablechange = null;
+  presentation.joinSession = function(url, id) {
+    // TODO: Reduce duplication between requestSession() and joinSession().
+    var session = new PresentationSession(url, id);
+    // If there is an existing session for the presentation, just return it.
+    if (presentationSessions_[session.key_]) {
+      return presentationSessions_[session.key_];
+    }
+    // Otherwise create a new session.
+    presentationSessions_[session.key_] = session;
+
+    // If the PresentationSession already has a Cast session attached return it.
+    if (session.hasCastSession_()) {
+      return session;
+    } else if (session.castSessionId_) {
+      // If the Cast session id is known for the PresentationSession, request
+      // it.
+      chrome.cast.lookupSessionById(session.castSessionId_);
+    } else {
+      // Must await for a session to be joined by the Cast SDK.
+    }
+    // Return the PresentationSession.
+    // TODO: Return a Promise that is resolved when the session is connected.
+    return session;
+  };
 
   var PresentationSession = function(url, id) {
     this.url = url;
@@ -69,7 +104,8 @@
     this.onmessage = null;
     this.onstatechange = null;
 
-    // Private properties
+    // Protected properties
+    this.key_ = url + '|' + id;
     this.origin_ = ORIGIN_RE_.exec(url)[0];
     this.castSessionId_ = null;
     this.castSession_ = null;
@@ -77,19 +113,33 @@
 
   PresentationSession.prototype.postMessage = function(message) {
     if (this.castSession_ && this.state == 'connected') {
+      log.info('postMessage to ' + this.key_ + ': ' + message);
       this.castSession_.sendMessage(PRESENTATION_API_NAMESPACE,
                                     message,
                                     null,
                                     this.close.bind(this));
+    } else {
+      log.warn('postMessage failed for ' + this.key_ +
+          '; no Cast session or not connected');
     }
   };
 
+  // NOTE: Should this return a Promise?
   PresentationSession.prototype.close = function() {
     if (this.state == 'disconnected') {
       return;
     }
     if (this.castSession_) {
-      this.castSession_.stop();
+      this.castSession_.leave(
+          function() {
+            log.info('Cast session ' + this.castSessionId_ +
+                ' left for presentation ' + this.key_);
+          }.bind(this),
+          function(error) {
+            log.error('Cast session ' + this.castSessionId_ +
+                ' for presentation ' + this.key_ + ' not left: ' +
+                JSON.stringify(error));
+          }.bind(this));
     }
     this.state = 'disconnected';
     this.fireStateChange_();
@@ -152,25 +202,6 @@
     // TODO(mfoltz): Check if the receiver is already displaying the URL.
     this.postMessage({cmd: 'open', url: this.url});
   };
-
-  var presentationSessions_ = {};
-  var requestedSession_ = null;
-
-  // var loadCastSessionIds_ = function() {
-  //   if (castSessionIds_) return;
-  //   var storedIds = localStorage['presentations'];
-  //   if (storedIds) {
-  //     castSessionIds_ = JSON.parse(storedIds);
-  //   } else {
-  //     castSessionIds_ = {};
-  //     localStorage['presentations'] = JSON.stringify(castSessionIds_);
-  //   }
-  // };
-
-  // var lookupCastSessionId_ = function(sessionKey) {
-  //   loadCastSessionIds_();
-  //   return castSessionIds_[presentationId] || null;
-  // };
 
 
   ////////////////////////////////////////////////////////////////////////////
