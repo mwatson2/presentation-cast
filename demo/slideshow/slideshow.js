1e1af36210ec71f98e4ed5cda7a921ad51cbb1dd
mark a. foltz <mfoltz@chromium.org>
2015-01-09 16:06:02 -0800

    Flesh out slideshow.js

--- demo/slideshow/slideshow.js
+++ demo/slideshow/slideshow.js
@@ -8,11 +8,39 @@
 
   // Utility for logging messages to the developer console.
   window.log = {
-    info: function(message) {console.info('[presentation_cast] ' + message);},
-    warn: function(message) {console.warn('[presentation_cast] ' + message);},
-    error: function(message) {console.error('[presentation_cast] ' + message);}
+    info: function(message) {console.info('[slideshow] ' + message);},
+    warn: function(message) {console.warn('[slideshow] ' + message);},
+    error: function(message) {console.error('[slideshow] ' + message);}
+  };
+  
+  var slideshow = {};
+
+  slideshow.init = function(photos) {
+    slideshow.photos_ = photos;
+    slideshow.elt_ = document.getElementById('photo_img');
+    slideshow.elt_.style.display = 'none';
+    slideshow.current_index_ = 0;
   };
 
-window.slideshow = foo;
+  slideshow.show = function(index) {
+    if (slideshow.elt_.style.display == 'none') {
+      slideshow.elt_.style.display = 'block';
+    }
+    log.info('Showing photo ' + index);
+    slideshow.elt_.src = slideshow.photos_[index];
+    slideshow.elt_.alt = 'Slideshow photo number ' + index;
+  };
+
+  slideshow.next = function() {
+    slideshow.current_index_ = (slideshow.current_index_ + 1) % slideshow.photos_.length;
+    slideshow.show(slideshow.current_index_);
+  };
+
+  slideshow.previous = function() {
+    slideshow.current_index_ = (slideshow.current_index_ - 1) % slideshow.photos_.length;
+    slideshow.show(slideshow.current_index_);
+  };
+
+  window.slideshow = slideshow;
 
 })();
